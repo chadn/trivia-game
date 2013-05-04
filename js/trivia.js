097b703885ce3c939afd153a63e6be36c6c1cbb7
Chad Norwood <chad@chadnorwood.com>
2013-05-04 12:20:21 -0500

    prototype complete ..

--- js/trivia.js
+++ js/trivia.js
@@ -1,32 +1,96 @@
 $(document).ready(function(){
-    var userName;
-    var socket = io.connect();
+    var socket;
     
-    socket.on('users', function (data) {
-      console.log('users updated, data:', data);
-    });
-    socket.on('users', function (data) {
-      $('#question').html(data.msg);
-    });
-    
-    $('#join button').on('click',function(){
-        userName = $('#join input').val();
+    function joinTrivia(userName) {
+        socket = io.connect();
+        console.log('io.connect socket:', socket);
+        
+        socket.on('users', function (data) {
+            console.log('users updated, data:', data);
+            $('.userMsg').html(data.msg);
+            updateUserStatus(data.users);
+        });
+
+        socket.on('question', function (data) {
+            console.log('received question: ', data);
+            if ($('#leave').css('display') == 'none') {
+                return;
+            }
+            updateQuestionHtml(data);
+        });
         socket.emit('userJoin', { 
             userName: userName
         });
-        
-        // update html 
+    }
+    function leaveTrivia() {
+        if (!socket) return;
+        socket.disconnect();
+        socket = null;
+    }
+    
+    function updateUserStatus(users) {
+        console.log('updateUserStatus', users);
+        var html = users.map(function(u){
+            return '<li>'+ u.name + ' points: '+ u.points +'</li>';
+        }).join('');
+        $('#status').html(users.length + " Active User"
+            + (users.length == 1 ? '' : 's')
+            + "<ol>"+ html + "</ol>");
+    }
+    
+    function updateQuestionHtml(data) {
+        var choicesHtml = '';
+        if (data.choices) {
+            for (var ii=0; ii < data.choices.length; ii++) {
+                choicesHtml += '<li>' + data.choices[ii] + '</li>';
+            }
+        }
+        var answerHtml = '';
+        if ('winner' in data) {
+            answerHtml += 'Correct Answer: ' + data.correctAnswer;
+            if (data.winner) {
+                answerHtml += '<br>YOU WON!! You were first to answer correctly.';
+            } else {
+                if (data.correctAnswer == $('#choices .selected').html()){
+                    answerHtml += '<br>Sorry. You were not the first to answer.';
+                } else {
+                    answerHtml += '<br>Sorry. You didn\'t guess correctly.';
+                }
+                if (data.winnerName) {
+                    answerHtml += '<br>Winner was ' + data.winnerName;
+                } else {
+                    answerHtml += '<br>Nobody won that one.';
+                }
+            }
+        } else {
+            $('#choices').html(choicesHtml);
+        }
+        $('#answer').html(answerHtml);
+        $('#question').html(data.question || '');
+    }
+    
+    $('#join button').on('click', function() {
+        var userName = $('#join input').val();
         $('.userName').html(userName);
         $('#join').hide();
         $('#leave').show();
+        joinTrivia(userName);
     });
     
     $('#leave button').on('click',function(){
-        socket.emit('userLeave');
-        // update html 
         $('#leave').hide();
         $('#join').show();
-        
+        //$('#status').html('');
+        updateQuestionHtml({});
+        leaveTrivia();
     });
+    $('#choices').on('click', 'li', function(){
+        $(this).addClass('selected');
+        console.log('clicked: ', $(this).html(), this);
+        socket.emit('answer', { 
+            answer: $(this).html(),
+            question: $('#question').html()
+        });
+    });  
     
 });
--- server.js
+++ server.js
@@ -4,16 +4,22 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
-    port = 8080,
-    url  = 'http://localhost:' + port + '/';
+    tq = require('./lib/TriviaQuestions.js'),
+    users = require('./lib/Users.js'),
+    MAX_NAME_LENGTH = 21;
+    PORT = 8080,
+    url  = 'http://localhost:' + PORT + '/';
 
-if(process.env.SUBDOMAIN){
+if (process.env.SUBDOMAIN) {
     url = 'http://' + process.env.SUBDOMAIN + '.jit.su/';
 }
 
-server.listen(port);
-console.log("Express server listening on port " + port);
+server.listen(PORT);
+console.log("Express server listening on port " + PORT);
 console.log(url);
+for (var nn=0; nn<100;nn++){
+    console.log(tq.getQuestion(true));    
+}
 
 app.use('/js/', express.static(__dirname + '/js'));
 app.get('/', function (req, res) {
@@ -21,33 +27,130 @@ app.get('/', function (req, res) {
 });
 
 var users = {};
+var points = {};
+var userCount = 0; // count of total users joined, not active users
+var winningSocket;
+
+var nextQuestionDelayMs = 3000; // how long are users 'warned' next question is coming
+var timeToAnswerMs = 7000; // how long users have to answer question
+var timeToEnjoyAnswerMs = 5000; // how long users have to read answer
+
 
 //Socket.io emits this event when a connection is made.
 io.sockets.on('connection', function (socket) {
 
     socket.on('userJoin', function (data) {
         console.log('SOCKET.IO user added: '+ data.userName + ' for socket '+ socket.id);
-        users[socket.id] = data.userName;
+        userCount++;
+        users[socket.id] = normalizeUsername(data.userName);
+        points[socket.id] = 0;
         emitUserUpdate(socket);
+        if (userCount == 1) {
+            // start game!
+            emitNewQuestion();
+        }
+    });
+    socket.on('disconnect', function() { 
+        console.log('SOCKET.IO user disconnect: '+ users[socket.id] + ' for socket '+ socket.id);
+        if (!users[socket.id]) {
+            // already disconnected
+            return;
+        }
+        delete users[socket.id];
+        emitUserUpdate();
     });
 
-    socket.on('userLeave', function () {
-        if (!users[socket.id]) return;
-        console.log('SOCKET.IO user removed: '+ users[socket.id] + ' for socket '+ socket.id);
-        delete users[socket.id];
-        emitUserUpdate(socket);
+    socket.on('answer', function (data) { 
+        console.log('SOCKET.IO user answered: "'+ data.answer + '" for question: '+ data.question);
+        // TODO: handle case where user might have already answered (damn hackers)
+        if (tq.isCorrect(data) && !winningSocket) {
+            console.log('SOCKET.IO user correct ! =========> : "'+ data.answer + '", '+ users[socket.id] + ' for socket '+ socket.id);
+            winningSocket = socket;
+        }
     });
 
 });
 
+function emitNewQuestion() {
+    winningSocket = null;
+
+    io.sockets.emit('question', {
+        endTime: new Date().getTime() + nextQuestionDelayMs,
+        question:'Next Question ...'
+    });
+
+    setTimeout(function(){
+        var q = tq.getQuestion(true);
+        q.endTime = new Date().getTime() + timeToAnswerMs;
+        
+        io.sockets.emit('question', q);
+        
+        setTimeout(function(){
+            emitAnswer();
+        }, timeToAnswerMs);
+
+    }, nextQuestionDelayMs);
+    
+}
+function emitAnswer() {
+    
+    var answerData = tq.getQuestion();
+    delete answerData.choices;
+    answerData.correctAnswer = tq.getAnswer();
+    answerData.endTime = new Date().getTime() + timeToEnjoyAnswerMs;
+    answerData.winner = false;
+    
+    if (winningSocket) {
+        answerData.winnerName = users[winningSocket.id];
+        points[winningSocket.id] += answerData.points;
+        emitUserUpdate();
+        
+        winningSocket.broadcast.emit('question', answerData); // emit to all but winner 
+
+        answerData.winner = true;
+        winningSocket.emit('question', answerData); // emit only to winner
+        
+    } else {
+        io.sockets.emit('question', answerData); // emit to everyone (no winner)
+    }
+    
+    setTimeout(function(){
+        emitNewQuestion();
+    }, timeToEnjoyAnswerMs);
+}
 
 function emitUserUpdate(socket) {
-    var userNames = [];
+    var userData = { 
+        users: [],
+    };
     for (var val in users){
-        userNames.push(users[val]);
+        userData.users.push({
+            points: points[val],
+            name: users[val]
+        });
     }
-    io.sockets.emit('users', { 
-        users: userNames,
-        msg: userNames.length + ' Active Users: '+ userNames.join(",\n") 
+    userData.users.sort(function(a,b) {
+        return b.points - a.points || a.name > b.name;
     });
+    
+    if (socket) {
+        socket.broadcast.emit('users', userData); // emit to all but socket 
+
+        userData.msg = 'Welcome, '+ users[socket.id];
+        socket.emit('users', userData); // emit only to socket
+        
+    } else {
+        io.sockets.emit('users', userData); // emit to everyone (points update)
+    }
 }
+function normalizeUsername(name) {
+    //name = 'Chad 42 "rocks"  the house';
+    name = name || '';
+    name = name.replace(/\s+/g,'_');
+    name = name.replace(/\W/g,'');
+    if (name.length > MAX_NAME_LENGTH) {
+        name = name.substring(0,MAX_NAME_LENGTH-2) + '_';
+    }
+    return name ? name : 'Player' + userCount;
+}
+
